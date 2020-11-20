423b11056a0e9b723be0a2f7064245139556c0c5
enrico <rombaldonienrico@gmail.com>
2020-11-20 11:43:42 +0100

    fix

--- js/script.js
+++ js/script.js
@@ -1,38 +1,72 @@
 //CAMPO MINATO
-/*Il computer deve generare 16 numeri casuali tra 1 e 100.
-I numeri non possono essere duplicati.
-In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
-L’utente non può inserire più volte lo stesso numero.
-Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
+/*
 La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
 Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.*/
+//Il computer deve generare 16 numeri casuali tra 1 e 100.
 
-// Il computer deve generare 16 numeri casuali tra 1 e 100.
-var minBot=1;
-var maxBot=100;
-var botRandomNumber= fxBotRadom(minBot,maxBot);
-console.log(botRandomNumber);
-fxControlDuplicate(botRandomNumber,minBot, maxBot);
-console.log(botRandomNumber);
 
-function fxBotRadom(min,max) {
-  var array=[];
-  for (var i = 0; i < 16; i++) {
-    array[i]=Math.floor(Math.random()*(max-min)+min);
+//Il computer deve generare 16 numeri casuali tra 1 e 100.
+//bombs
+
+var bootNumbers=[];
+//Il computer deve generare 16 numeri casuali tra 1 e 100.
+while(bootNumbers.length < 16 ){
+  var numbers= Math.floor(Math.random()*(100-1))+1;
+//I numeri non possono essere duplicati.
+  if (alreadyexist(numbers,bootNumbers)){
+
+    alreadyexist(numbers,bootNumbers);
+
+  } else {
+
+  bootNumbers.push(numbers);
   }
-  return array;
 }
 
 
+console.log(bootNumbers);
+
+
+var userNumbers= [];
+//In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
+var score=0;
+while(userNumbers.length < 16 ){
+  var numberUser= parseInt(prompt("inserisci numero"))
+//L’utente non può inserire più volte lo stesso numero.
+  if (alreadyexist(numberUser,userNumbers)){
+
+    alreadyexist(numberUser,userNumbers);
+
+  }else if (alreadyexist(numberUser,bootNumbers)){
+    //La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
+
+    alert("hai perso");
+    break;
+
+  }else {
+
+  userNumbers.push(numberUser);
+  score=userNumbers.length;
+  }
+
+}
+console.log(userNumbers);
+console.log(score);
+
+
+//Il computer deve generare 16 numeri casuali tra 1 e 100.
+
+
+
+
 //funzioni di controllo
-function fxControlDuplicate(arg1,min,max) {
-  //controllo dupplicati
-  arg1.sort();
-  for (var j = 0; j < arg1.length; j++) {
-    while (arg1[j]== arg1[j-1]) {
-      arg1[j]=Math.floor(Math.random()*(max-min)+min);
-      arg1.sort();
+//I numeri non possono essere duplicati.
+function alreadyexist(numeroinserito, array) {
+  var trovato=false;
+  for (var i = 0; i < array.length; i++) {
+    if(array[i]==numeroinserito){
+      return trovato=true;
     }
   }
-  return arg1;
+  return trovato;
 }

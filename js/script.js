79191f957482bf5b2c06af6fd9f8657f27411ba0
enrico <rombaldonienrico@gmail.com>
2020-11-20 13:22:04 +0100

    bonus

--- js/script.js
+++ js/script.js
@@ -1,17 +1,28 @@
 //CAMPO MINATO
-/*
-La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
-Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.*/
-//Il computer deve generare 16 numeri casuali tra 1 e 100.
-
-
 //Il computer deve generare 16 numeri casuali tra 1 e 100.
 //bombs
+/*BONUS: (da fare solo se funziona tutto il resto)
+all’inizio il software richiede anche una difficoltà all’utente che cambia il range di numeri casuali:
+con difficoltà 0 => tra 1 e 100
+con difficoltà 1 =>  tra 1 e 80
+con difficoltà 2 => tra 1 e 50*/
+var level=parseInt(prompt("Insierisci livello"));
+switch (level) {
+  case 1:
+    max=80;
+    break;
+  case 2:
+    max=50;
+  default:
+    max=100;
+
+}
+max-=16;
 
 var bootNumbers=[];
 //Il computer deve generare 16 numeri casuali tra 1 e 100.
 while(bootNumbers.length < 16 ){
-  var numbers= Math.floor(Math.random()*(100-1))+1;
+  var numbers= Math.floor(Math.random()*(max-1))+1;
 //I numeri non possono essere duplicati.
   if (alreadyexist(numbers,bootNumbers)){
 
@@ -45,16 +56,16 @@ while(userNumbers.length < 16 ){
 
   }else {
 
-  userNumbers.push(numberUser);
-  score=userNumbers.length;
+    userNumbers.push(numberUser);
+    score=userNumbers.length;
   }
 
 }
 console.log(userNumbers);
+//Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero
 console.log(score);
 
 
-//Il computer deve generare 16 numeri casuali tra 1 e 100.
 
 
 
